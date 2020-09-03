a86b1a227a5208f5362e3804d55ee4eaaf535877
Félix Lesc <felix.maneville@gmail.com>
2020-09-03 10:08:45 +0000

    Added missing lib.rs documentation Added missing modules and functions documentations Improved Readme

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -1,2 +1,8 @@
+/// Collection of helper functions validating string formats.
+///
+/// The functions are made to be used with the [`Authenticate`] trait.
+///
+/// [`Authenticate`]: ../../trait.Authenticate.html
 pub mod string_validators;
+/// Tools to extract `serde_json`::`Value` data
 pub mod json_helper;
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,149 @@
+//! `aragog` is a simple lightweight ODM library for [ArangoDB][ArangoDB] using the [arangors][arangors] driver.
+//! The main concept is to provide behaviors allowing to synchronize documents and structs as simply an lightly as possible.
+//! In the future versions `aragog` will also be able to act as a ORM and OGM for [ArangoDB][ArangoDB]
+//!
+//! ### Features
+//!
+//! By now the available features are:
+//! * Creating a database connection pool from a defined `schema.json`
+//! * Structures can implement different behaviors:
+//! * `Record`: The structure can be written into a ArangoDB collection as well as retrieved, from its `_key` or other query arguments.
+//! * `New`: The structure can be initialized from an other type (a form for example). It allows to maintain a privacy level in the model and to use different data formats.
+//! * `Update`: The structure can be updated from an other type (a form for example). It allows to maintain a privacy level in the model and to use different data formats.
+//! * `Validate`: The structure can perform simple validations before being created or saved into the database.
+//! * `Authenticate`: The structure can define a authentication behaviour from a `secret` (a password for example)
+//! * Different operations can return a `ServiceError` error that can easily be transformed into a Http Error (can be used for the actix framework)
+//!
+//! #### Cargo features
+//!
+//! ##### Actix Http Error
+//!
+//! If you use this crate with the [actix][actix] framework, you may want the `aragog` errors to be usable as http errors.
+//! To do so cou can add to your `cargo.toml` the following `feature`: `actix_http_error`.
+//!
+//! ```toml
+//! aragog = { version = "0.2.0", features = ["actix_http_error"] }
+//! ```
+//!
+//! ### Schema and collections
+//!
+//! In order for everything yo work you need to specify a `schema.json` file. The path of the schema must be set in `SCHEMA_PATH` environment variable or by default the pool will look for it in `src/config/db/schema.json`.
+//! > There is an example `schema.json` file in [/examples/simple_food_order_app][example_path]
+//!
+//! The json must look like this:
+//!
+//! ```json
+//! {
+//!    "collections": [
+//!        {
+//!            "name": "collection1",
+//!            "indexes": []
+//!        },
+//!        {
+//!            "name": "collection2",
+//!            "indexes": [
+//!                {
+//!                    "name": "byUsernameAndEmail",
+//!                    "fields": ["username", "email"],
+//!                    "settings": {
+//!                        "type": "persistent",
+//!                        "unique": true,
+//!                        "sparse": false,
+//!                        "deduplicate": false
+//!                    }
+//!                }
+//!            ]
+//!        }
+//!    ]
+//! }
+//! ```
+//!
+//! When initializing the `DatabaseConnectionPool` every collection `name` will be searched in the database and if not found the collection will be automatically created.
+//! > You don't need to create the collections yourself
+//!
+//! ##### Indexes
+//!
+//! The array of Index in `indexes` must have that exact format:
+//! * `name`: the index name,
+//! * `fields`: an array of the fields concerned on that compound index,
+//! * `settings`: this json bloc must be the serialized version of an [IndexSettings][IndexSettings] variant from [arangors][arangors] driver.
+//!
+//! #### Database Record
+//!
+//! The global architecture is simple, every *Model* you define that can be synced with the database must implement `Record` and derive from `serde::Serialize`, `serde::Deserialize` and `Clone`.
+//! If you want any of the other behaviors you can implement the associated *trait*
+//!
+//! The final *Model* structure will be an **Exact** representation of the content of a ArangoDB *document*, so without its `_key`, `_id` and `_rev`.
+//! Your project should contain some `models` folder with every `struct` representation of your database documents.
+//!
+//! The real representation of a complete document is `DatabaseRecord<T>` where `T` is your model structure.
+//!
+//! **Example:**
+//!
+//! ```rust compile_fail
+//! # use serde::{Deserialize, Serialize};
+//! # use aragog::{Record, DatabaseRecord, DatabaseConnectionPool};
+//! #
+//! #[derive(Serialize, Deserialize, Clone)]
+//! pub struct User {
+//!     pub username: String,
+//!     pub first_name: String,
+//!     pub last_name: String,
+//! }
+//!
+//! impl Record for User {
+//!     fn collection_name() -> String {
+//!         String::from("Users")
+//!     }
+//! }
+//!
+//! async fn main() {
+//!     // Database connection Setup
+//!     let database_pool = DatabaseConnectionPool::new("http://localhost:8529", "db", "root", "").await;
+//!     // Define a document
+//!     let mut user = User {
+//!         username: String::from("LeRevenant1234"),
+//!         first_name: String::from("Robert"),
+//!         last_name: String::from("Surcouf")
+//!     };
+//!     // user_record is a DatabaseRecord<User>
+//!     let user_record = DatabaseRecord::create(user, &database_pool).await;
+//!     // You can access and edit the document
+//!     user_record.record.username = String::from("LeRevenant1524356");
+//!     // And directly save it
+//!     user_record.save(&database_pool).await;
+//! }
+//! ```
+//!
+//! #### Querying
+//!
+//! You can retrieve a document from the database as simply as it gets, from the unique ArangoDB `_key` or from multiple conditions.
+//! The example below show different ways to retrieve records, look at each function documentation for more exhaustive exaplanations.
+//!
+//! **Example**
+//! ```rust compile_fail
+//! // Find with the primary key
+//! let user_record = User::find("1234567", &database_pool).await.unwrap();
+//!
+//! // Find with a single condition
+//! let user_record = User::find_by("username" ,"LeRevenant1234", &database_pool).await.unwrap();
+//!
+//! // Find a user with multiple conditions
+//! let mut find_conditions :Vec<&str> = Vec::new();
+//! find_conditions.push(r#"username == "LeRevenant1234""#);
+//! find_conditions.push(r#"last_name == "Surcouf""#);
+//! find_conditions.push("age > 15");
+//! let user_record = User::find_where(find_conditions, &database_pool).await.unwrap();
+//!
+//! // Find all users with multiple conditions
+//! let mut find_conditions :Vec<&str> = Vec::new();
+//! find_conditions.push(r#"username == "LeRevenant1234""#);
+//! find_conditions.push(r#"last_name == "Surcouf""#);
+//! find_conditions.push("age > 15");
+//! let user_records = User::get_where(find_conditions, &database_pool).await.unwrap();
+//! ```
+#![warn(missing_docs)]
+
 pub use {
     authenticate::Authenticate,
     db::database_record::DatabaseRecord,
@@ -9,6 +155,7 @@ pub use {
     validate::Validate,
 };
 
+/// Contains useful tools to parse json value and to valiate string formats
 pub mod helpers;
 mod db;
 mod record;
@@ -17,4 +164,3 @@ mod update;
 mod validate;
 mod new;
 mod error;
-
