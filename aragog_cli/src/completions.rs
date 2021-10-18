afd7e312d6c037611d72646e4efb2b44be34093a
Felix de Maneville <felix.maneville@gmail.com>
2021-10-18 14:45:03 +0200

    clap v3 beta5 update

--- aragog_cli/src/completions.rs
+++ aragog_cli/src/completions.rs
@@ -1,15 +1,15 @@
 use crate::app::AragogCliApp;
-use clap::{App, ArgEnum, Clap, IntoApp};
+use clap::{App, ArgEnum, IntoApp, Parser};
 use clap_generate::generators::*;
 use clap_generate::{generate, Generator};
 
-#[derive(Clap, Debug)]
+#[derive(Parser, Debug)]
 pub struct CompletionOptions {
     /// target shell type
     #[clap(arg_enum, default_value = "bash")]
     pub shell_type: ShellType,
 }
-#[derive(Clap, ArgEnum, Debug)]
+#[derive(Parser, ArgEnum, Debug, Copy, Clone)]
 pub enum ShellType {
     Bash,
     Elvish,
@@ -19,20 +19,20 @@ pub enum ShellType {
     Zsh,
 }
 
-fn print_completions<G: Generator>(app: &mut App) {
+fn print_completions<G: Generator>(generator: G, app: &mut App) {
     let name = app.get_name().to_string();
-    generate::<G, _>(app, name, &mut std::io::stdout());
+    generate(generator, app, name, &mut std::io::stdout());
 }
 
 impl CompletionOptions {
     pub fn generate(&self) {
         let mut app = AragogCliApp::into_app();
         match self.shell_type {
-            ShellType::Bash => print_completions::<Bash>(&mut app),
-            ShellType::Elvish => print_completions::<Elvish>(&mut app),
-            ShellType::Fish => print_completions::<Fish>(&mut app),
-            ShellType::PowerShell => print_completions::<PowerShell>(&mut app),
-            ShellType::Zsh => print_completions::<Zsh>(&mut app),
+            ShellType::Bash => print_completions(Bash, &mut app),
+            ShellType::Elvish => print_completions(Elvish, &mut app),
+            ShellType::Fish => print_completions(Fish, &mut app),
+            ShellType::PowerShell => print_completions(PowerShell, &mut app),
+            ShellType::Zsh => print_completions(Zsh, &mut app),
         };
     }
 }
